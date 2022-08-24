dfd900791ab41647f59f3227241c55fb90154d4f
João Machado <joao.vicente@evolutix.com.br>
2022-08-24 12:04:29 -0300

    temp: created  grid with graphics

--- src/app/modules/order/components/produtividade/order-graphics-view/order-graphics-view.component.ts
+++ src/app/modules/order/components/produtividade/order-graphics-view/order-graphics-view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { SingleDataStatistics } from "./../../../../nps/components/nps-page/nps-page.component";
+import { CHART_TYPES } from "./../../../../dynamic-widgets/charts/charts.component";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-order-graphics-view',
-  templateUrl: './order-graphics-view.component.html',
-  styleUrls: ['./order-graphics-view.component.scss']
+  selector: "app-order-graphics-view",
+  templateUrl: "./order-graphics-view.component.html",
+  styleUrls: ["./order-graphics-view.component.scss"],
 })
 export class OrderGraphicsViewComponent implements OnInit {
+  halfHourData: SingleDataStatistics[] = [];
+  topFiveOppened: SingleDataStatistics[] = [];
+  afterSlaData: SingleDataStatistics[] = [];
 
-  constructor() { }
+  CHART_TYPES = CHART_TYPES;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
