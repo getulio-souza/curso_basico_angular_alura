eb7e5903f44a3f080af254e64e8a5e57b7a006b6
João Machado <joao.vicente@evolutix.com.br>
2022-08-25 15:29:27 -0300

    pulled imports from table component

--- src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
+++ src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
@@ -1,5 +1,8 @@
+import { OrderEventService } from "./../../../../../services/order/order-event.service";
 import { Component, OnInit } from "@angular/core";
 import { CollumnDefinition } from "../../../../dynamic-widgets/collapse-grid/collapse-grid.component";
+import { OrderWardQuantityDTO } from "../../../model/ordar-ward-quantity.dto";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-order-table",
@@ -14,7 +17,29 @@ export class OrderTableComponent implements OnInit {
 
   data = [];
 
-  constructor() {}
+  private loadingSubject: Subject<void> = new Subject<void>();
+
+  constructor(private orderEventService: OrderEventService) {}
 
   ngOnInit() {}
+
+  private topFiveWardsAndOppenedOrdersQuantityForTable(
+    ward,
+    category,
+    subject,
+    start,
+    end
+  ): void {
+    this.orderEventService
+      .topFiveWardsAndOppenedOrdersQuantity(ward, category, subject, start, end)
+      .subscribe((response: OrderWardQuantityDTO[]) => {
+        this.data = response.map((ward) => {
+          this.loadingSubject.next();
+          return Object.assign(
+            {},
+            { ward: ward.ward, quantity: ward.quantity }
+          );
+        });
+      });
+  }
 }
