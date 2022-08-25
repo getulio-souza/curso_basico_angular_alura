411a20fbf3bfc8c38a37cdce241325db8aab129b
João Machado <joao.vicente@evolutix.com.br>
2022-08-25 11:30:28 -0300

    feature: pulled imports to graphic component

--- src/app/modules/order/components/order-productivity/order-graphics-view/order-graphics-view.component.ts
+++ src/app/modules/order/components/order-productivity/order-graphics-view/order-graphics-view.component.ts
@@ -1,6 +1,12 @@
+import { OrderEventService } from "./../../../../../services/order/order-event.service";
+import { OrderEvent } from "../../../model/order-event";
 import { SingleDataStatistics } from "../../../../nps/components/nps-page/nps-page.component";
 import { CHART_TYPES } from "../../../../dynamic-widgets/charts/charts.component";
 import { Component, OnInit } from "@angular/core";
+import { OrderCategoriesWardsSubjectsAndSectorsDTO } from "../../../model/order-categories-wards-and-subjects.dto";
+import { OrderCategoryQuantityDTO } from "../../../model/order-category-quantity.dto";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: "app-order-graphics-view",
@@ -13,6 +19,16 @@ export class OrderGraphicsViewComponent implements OnInit {
   afterSlaData: SingleDataStatistics[] = [];
 
   CHART_TYPES = CHART_TYPES;
+
+  seletorWards: string[] = [];
+  seletorSubjects: string[] = [];
+
+  cacheWard = null;
+  cacheSubject = null;
+
+  private loadingSubject: Subject<void> = new Subject<void>();
+
+  loading = false;
 
   teste = [
     {
@@ -37,7 +53,123 @@ export class OrderGraphicsViewComponent implements OnInit {
     },
   ];
 
-  constructor() {}
+  constructor(private orderEventService: OrderEventService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadingSubject.pipe(debounceTime(200)).subscribe((_) => {
+      this.loading = false;
+    });
+  }
+
+  halfChartClickEvent(event: { to: string; value: any; extras: any }): void {
+    if (event.to === "category" || event.to === null) {
+      this.seletorSubjects = [];
+      this.cacheWard = event.extras.ward;
+      this.cacheSubject = event.extras.subject;
+      this.findEventsCategoriesOpennedAtHalfhour(
+        event.extras.ward,
+        null,
+        event.extras.subject,
+        null,
+        null
+      );
+    }
+    if (event.to === "item") {
+      this.findEventsOpennedAtHalfhour(
+        event.extras.ward,
+        event.value,
+        event.extras.subject,
+        null,
+        null
+      );
+    }
+  }
+
+  selectedFilterEvent(event): void {
+    if (!event.subject) {
+      this.orderEventService
+        .getSubjectsByWard(event.ward)
+        .subscribe((response: OrderCategoriesWardsSubjectsAndSectorsDTO) => {
+          this.seletorSubjects = response.subjects;
+        });
+    }
+
+    this.findEventsCategoriesOpennedAtHalfhour(
+      event.ward,
+      null,
+      event.subject,
+      null,
+      null
+    );
+  }
+
+  private findEventsCategoriesOpennedAtHalfhour(
+    ward,
+    category,
+    subject,
+    start,
+    end
+  ): void {
+    this.orderEventService
+      .findEventsCategoriesOpennedAtHalfhour(
+        ward,
+        category,
+        subject,
+        start,
+        end
+      )
+      .subscribe((response: OrderCategoryQuantityDTO[]) => {
+        this.halfHourData = response.map((category) => {
+          this.loadingSubject.next();
+          return Object.assign(
+            {},
+            {
+              label: category.category,
+              value: [category.quantity, category.min, category.max],
+            }
+          );
+        });
+      });
+  }
+
+  private findEventsOpennedAtHalfhour(
+    ward,
+    category,
+    subject,
+    start,
+    end
+  ): void {
+    this.orderEventService
+      .findEventsOpennedAtHalfhour(ward, category, subject, start, end)
+      .subscribe((events: OrderEvent[]) => {
+        this.halfHourData = [];
+        if (events.length) {
+          const itens = new Set(events.map((item) => item.item));
+          itens.forEach((item) => {
+            const label = events.find((obj) => obj.item === item).labels
+              ? events.find((obj) => obj.item === item).labels["pt"]
+              : null;
+            const quantity = events
+              .filter((obj) => obj.item === item)
+              .map((obj) => parseInt(obj.quantity))
+              .reduce((act, nxt) => act + nxt);
+            const min = events
+              .filter((obj) => obj.item === item)
+              .map((obj) => obj.timestamp)
+              .reduce((prv, crt) => {
+                return prv > crt ? crt : prv;
+              });
+            const max = events
+              .filter((obj) => obj.item === item)
+              .map((obj) => obj.timestamp)
+              .reduce((prv, crt) => {
+                return !(prv > crt) ? crt : prv;
+              });
+            this.halfHourData.push(
+              Object.assign({}, { label, value: [quantity, max, min] })
+            );
+          });
+        }
+      });
+  }
 }
