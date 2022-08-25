f0a044cbbb48b6c3eea5a9738f6c290dce031da3
João Machado <joao.vicente@evolutix.com.br>
2022-08-25 15:16:34 -0300

    call data to table

--- src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
+++ src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CollumnDefinition } from "../../../../dynamic-widgets/collapse-grid/collapse-grid.component";
 
 @Component({
-  selector: 'app-order-table',
-  templateUrl: './order-table.component.html',
-  styleUrls: ['./order-table.component.scss']
+  selector: "app-order-table",
+  templateUrl: "./order-table.component.html",
+  styleUrls: ["./order-table.component.scss"],
 })
 export class OrderTableComponent implements OnInit {
+  cols: CollumnDefinition[] = [
+    { field: "ward", header: "Ala" },
+    { field: "quantity", header: "Quantidade" },
+  ];
 
-  constructor() { }
+  data = [];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
