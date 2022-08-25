86923801de8a9b900ced8cb066d2130406ad918a
Odair Viol <odair.viol@gmail.com>
2022-08-25 18:28:38 +0000

    Revert "Merge branch 'feature/order-refactory' into 'develop'"
    This reverts merge request !4

--- src/app/modules/dynamic-widgets/charts/components/chart-pizza/chart-pizza.component.ts
+++ src/app/modules/dynamic-widgets/charts/components/chart-pizza/chart-pizza.component.ts
@@ -1,40 +1,31 @@
-import { SingleDataStatistics } from "../../../../order/components/order-productivity/order-productivity.component";
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from "@angular/core";
+import { SingleDataStatistics } from './../../../../order/components/produtividade/produtividade.component';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from "@angular/core";
 import { EChartOption } from "echarts";
-import * as echarts from "echarts";
+import * as echarts from 'echarts';
 @Component({
-  selector: "app-chart-pizza",
-  templateUrl: "./chart-pizza.component.html",
+    selector: 'app-chart-pizza',
+    templateUrl: './chart-pizza.component.html'
 })
 export class ChartPizzaComponent implements OnInit {
-  private static CATEGORY = "category";
-  private static WARD = "ward";
-  private static ITEM = "item";
+  private static CATEGORY = 'category';
+  private static WARD = 'ward';
+  private static ITEM = 'item';
 
   private actual = ChartPizzaComponent.CATEGORY;
 
-  @Output() clickPizzaEvent: EventEmitter<{ to: string; value: any }> =
-    new EventEmitter<{ to: string; value: any }>();
-  @ViewChild("chartPizzaCanvas", { static: true })
-  chartPizzaCanvas: ElementRef<HTMLDivElement>;
+  @Output() clickPizzaEvent: EventEmitter<{to: string, value: any}> = new EventEmitter<{to: string, value: any}>();
+  @ViewChild('chartPizzaCanvas', { static: true }) chartPizzaCanvas: ElementRef<HTMLDivElement>;
 
   private chart: echarts.ECharts;
 
-  private subtext: string = "Categoria";
+  private subtext: string = 'Categoria';
   private filterCache: string = null;
 
   ngOnInit(): void {
     this.initChart();
-    window.addEventListener("resize", () => this.chart.resize());
+    window.addEventListener('resize', () => this.chart.resize());
 
-    this.chart.on("click", (handler) => {
+    this.chart.on('click', (handler) => {
       if (this.actual === ChartPizzaComponent.CATEGORY) {
         this.actual = ChartPizzaComponent.WARD;
         this.subtext = "Ala / Departamento";
@@ -43,12 +34,12 @@ export class ChartPizzaComponent implements OnInit {
         this.actual = ChartPizzaComponent.ITEM;
         this.subtext = "Itens";
         this.filterCache = `${this.filterCache} > ${handler.name}`;
-      } else if (this.actual === ChartPizzaComponent.ITEM) {
+      } else  if (this.actual === ChartPizzaComponent.ITEM) {
         this.actual = ChartPizzaComponent.CATEGORY;
         this.subtext = "Categoria";
         this.filterCache = null;
       }
-      this.clickPizzaEvent.emit({ to: this.actual, value: handler.name });
+      this.clickPizzaEvent.emit({to: this.actual, value: handler.name});
     });
   }
 
@@ -59,52 +50,47 @@ export class ChartPizzaComponent implements OnInit {
   async configureChart(itens: SingleDataStatistics[]): Promise<void> {
     this.clearChart();
 
-    const series = itens.map((item) =>
-      Object.assign(
-        {},
-        { name: item.label, value: item.value[0], label: { color: "#FFFFFF" } }
-      )
-    );
+    const series = itens.map(item => Object.assign({}, {name: item.label, value: item.value[0], label: {color: '#FFFFFF'}}));
 
     const chartPizzaOption: EChartOption = {
       title: {
         text: this.subtext,
-        right: "right",
+        right: 'right',
         textStyle: {
-          color: "#FFFFFF",
-          fontSize: 15,
+          color: '#FFFFFF',
+          fontSize: 15
         },
         subtext: this.filterCache,
         subtextStyle: {
-          color: "#CCCCCC",
-          fontSize: 12,
-        },
+          color: '#CCCCCC',
+          fontSize: 12
+        }
       },
       tooltip: {
-        trigger: "item",
+        trigger: 'item'
       },
       legend: {
-        orient: "vertical",
-        left: "left",
+        orient: 'vertical',
+        left: 'left',
         textStyle: {
-          color: "#FFFFFF",
-        },
+          color: '#FFFFFF'
+        }
       },
       series: [
         {
-          name: "Quantidade",
-          type: "pie",
-          radius: "70%",
+          name: 'Quantidade',
+          type: 'pie',
+          radius: '70%',
           data: [...series],
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
               shadowOffsetX: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)",
-            },
-          },
-        },
-      ],
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
     };
 
     this.chart.setOption(chartPizzaOption);
--- src/app/modules/starter-page/starter-page.module.ts
+++ src/app/modules/starter-page/starter-page.module.ts
@@ -1,159 +1,93 @@
-import { PropertySummaryPageComponent2 } from "./../property-summary/components/property-summary-page-2/property-summary-page-2.component";
-import { HomePage2Component } from "./../home/components/homePage2/homePage2.component";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { CallbackComponent } from "./components/callback/callback.component";
-import { PropertiesCardComponent } from "./components/properties-card/properties-card.component";
-import { SelectPropertyComponent } from "./components/select-property/select-property.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { NotFoundComponent } from "./components/not-found/not-found.component";
-import { PropertyAppComponent } from "./components/property-app/property-app.component";
-import { TranslateModule } from "@ngx-translate/core";
-import { HeaderStatusComponent } from "./components/header-status/header-status.component";
-import { RouterModule } from "@angular/router";
-import { AuthGuard } from "../../guards/auth-guard/auth.guard";
-import { NoRegisteredGuard } from "../../guards/noRegistered-guard/noRegistered.guard";
-import { PropertySummaryPageComponent } from "../../propertySummaryPage/property-summary-page/property-summary-page.component";
-import { ConsumeDetailsComponent } from "../../consume-details/consume-details.component";
-import { OccupancyDetailsComponent } from "../../occupancy-details/occupancy-details.component";
-import { EngineeringPageComponent } from "../../engineering/engineering-page/engineering-page.component";
-import { HousekeepingPageComponent } from "../../houseKeeping/housekeepingPage/housekeepingPage.component";
-import { OptimizationPageComponent } from "../../optimization/optimizationPage/optimizationPage.component";
-import { TelkonetPageComponent } from "../../telkonet/telkonet-page/telkonet-page.component";
-import { MaintenancePageComponent } from "../../maintenance/maintenancePage/maintenancePage.component";
-import { ITPageComponent } from "../../it/itPage/itPage.component";
-import { OccupancyWeekDetailsComponent } from "../../occupancy-week-details/occupancy-week-details.component";
-import { ConsumeWeekDetailsComponent } from "../../consume-week-details/consume-week-details.component";
-import { NotificationMessagesComponent } from "./components/notification-messages/notification-messages.component";
-import { ProxperConfigGuard } from "../../guards/proxperConfig-guard/proxperConfig.guard";
-import { NoRegisteredPropertiesComponent } from "./components/no-registered-properties/no-registered-properties.component";
-import { CRMPageComponent } from "../crm/components/crmPage/crmPage.component";
-import { UsageReportPageComponent } from "../usageReport/components/usageReportPage/usageReportPage.component";
-import { OrderPageComponent } from "../order/components/order-page/order-page.component";
-import { RoleGuard } from "../../guards/role-guard/role.guard";
-import { RoomControlPage } from "../room-control/components/room-control-page/room-control-page.component";
-import { OrderProductivityComponent } from "../order/components/order-productivity/order-productivity.component";
-import { NvD3Module } from "ng2-nvd3";
-import { NPSPageComponent } from "../nps/components/nps-page/nps-page.component";
-import { LoginComponent } from "../../login/login.component";
+import { PropertySummaryPageComponent2 } from './../property-summary/components/property-summary-page-2/property-summary-page-2.component';
+import { HomePage2Component } from './../home/components/homePage2/homePage2.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CallbackComponent } from './components/callback/callback.component';
+import { PropertiesCardComponent } from './components/properties-card/properties-card.component';
+import { SelectPropertyComponent } from './components/select-property/select-property.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PropertyAppComponent } from './components/property-app/property-app.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { HeaderStatusComponent } from './components/header-status/header-status.component';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from '../../guards/auth-guard/auth.guard';
+import { NoRegisteredGuard } from '../../guards/noRegistered-guard/noRegistered.guard';
+import { PropertySummaryPageComponent } from '../../propertySummaryPage/property-summary-page/property-summary-page.component';
+import { ConsumeDetailsComponent } from '../../consume-details/consume-details.component';
+import { OccupancyDetailsComponent } from '../../occupancy-details/occupancy-details.component';
+import { EngineeringPageComponent } from '../../engineering/engineering-page/engineering-page.component';
+import { HousekeepingPageComponent } from '../../houseKeeping/housekeepingPage/housekeepingPage.component';
+import { OptimizationPageComponent } from '../../optimization/optimizationPage/optimizationPage.component';
+import { TelkonetPageComponent } from '../../telkonet/telkonet-page/telkonet-page.component';
+import { MaintenancePageComponent } from '../../maintenance/maintenancePage/maintenancePage.component';
+import { ITPageComponent } from '../../it/itPage/itPage.component';
+import { OccupancyWeekDetailsComponent } from '../../occupancy-week-details/occupancy-week-details.component';
+import { ConsumeWeekDetailsComponent } from '../../consume-week-details/consume-week-details.component';
+import { NotificationMessagesComponent } from './components/notification-messages/notification-messages.component';
+import { ProxperConfigGuard } from '../../guards/proxperConfig-guard/proxperConfig.guard';
+import { NoRegisteredPropertiesComponent } from './components/no-registered-properties/no-registered-properties.component';
+import { CRMPageComponent } from '../crm/components/crmPage/crmPage.component';
+import { UsageReportPageComponent } from '../usageReport/components/usageReportPage/usageReportPage.component';
+import { OrderPageComponent } from '../order/components/order-page/order-page.component';
+import { RoleGuard } from '../../guards/role-guard/role.guard';
+import { RoomControlPage } from '../room-control/components/room-control-page/room-control-page.component';
+import { ProdutividadePageComponent } from '../order/components/produtividade/produtividade.component';
+import { NvD3Module } from 'ng2-nvd3';
+import { NPSPageComponent } from '../nps/components/nps-page/nps-page.component';
+import { LoginComponent } from '../../login/login.component';
 // import { NgxEchartsModule } from 'ngx-echarts';
+
 
 @NgModule({
   imports: [
+
     CommonModule,
     TranslateModule.forChild(),
     RouterModule.forChild([
       {
-        path: "login",
-        component: LoginComponent,
+        path: 'login',
+        component: LoginComponent
       },
       {
-        path: "selectProperty",
+        path: 'selectProperty',
         component: SelectPropertyComponent,
-        canActivate: [AuthGuard],
+        canActivate: [AuthGuard]
       },
       {
-        path: "noRegisteredProperty",
+        path: 'noRegisteredProperty',
         component: NoRegisteredPropertiesComponent,
-        canActivate: [AuthGuard, NoRegisteredGuard],
+        canActivate: [AuthGuard,NoRegisteredGuard]
       },
       {
-        path: "propertyApp/:propertyId",
+        path: 'propertyApp/:propertyId',
         component: PropertyAppComponent,
         canActivate: [AuthGuard, ProxperConfigGuard],
         children: [
-          { path: "", component: HomePage2Component },
-          { path: "home", component: HomePage2Component },
-          {
-            path: "propertySummary",
-            component: PropertySummaryPageComponent2,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "propertySummaryold",
-            component: PropertySummaryPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "consumeDetails/:energyGroupId",
-            component: ConsumeDetailsComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "consumeWeekDetails/:energyGroupId",
-            component: ConsumeWeekDetailsComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "occupancyDetails/:energyGroupId",
-            component: OccupancyDetailsComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "occupancyWeekDetails/:energyGroupId",
-            component: OccupancyWeekDetailsComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "engineering",
-            component: EngineeringPageComponent,
-            canActivate: [RoleGuard],
-          },
-          { path: "it", component: ITPageComponent, canActivate: [RoleGuard] },
-          {
-            path: "maintenance",
-            component: MaintenancePageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "housekeeping",
-            component: HousekeepingPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "optimization",
-            component: OptimizationPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "energyManagement",
-            component: TelkonetPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "crm",
-            component: CRMPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "usageReport",
-            component: UsageReportPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "order",
-            component: OrderPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "nps",
-            component: NPSPageComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "produtividade",
-            component: OrderProductivityComponent,
-            canActivate: [RoleGuard],
-          },
-          {
-            path: "roomControl",
-            component: RoomControlPage,
-            canActivate: [RoleGuard],
-          },
-        ],
-      },
+          { path: '', component: HomePage2Component},
+          { path: 'home', component: HomePage2Component},
+          { path: 'propertySummary', component: PropertySummaryPageComponent2, canActivate: [RoleGuard]},
+          { path: 'propertySummaryold', component: PropertySummaryPageComponent, canActivate: [RoleGuard]},
+          { path: 'consumeDetails/:energyGroupId', component: ConsumeDetailsComponent, canActivate: [RoleGuard]},
+          { path: 'consumeWeekDetails/:energyGroupId', component: ConsumeWeekDetailsComponent , canActivate: [RoleGuard]},
+          { path: 'occupancyDetails/:energyGroupId', component: OccupancyDetailsComponent, canActivate: [RoleGuard]},
+          { path: 'occupancyWeekDetails/:energyGroupId', component: OccupancyWeekDetailsComponent, canActivate: [RoleGuard]},
+          { path: 'engineering', component: EngineeringPageComponent , canActivate: [RoleGuard]},
+          { path: 'it', component: ITPageComponent, canActivate: [RoleGuard]},
+          { path: 'maintenance', component: MaintenancePageComponent, canActivate: [RoleGuard]},
+          { path: 'housekeeping', component: HousekeepingPageComponent, canActivate: [RoleGuard]},
+          { path: 'optimization', component: OptimizationPageComponent, canActivate: [RoleGuard]},
+          { path: 'energyManagement', component: TelkonetPageComponent, canActivate: [RoleGuard]},
+          { path: 'crm', component: CRMPageComponent, canActivate: [RoleGuard]},
+          { path: 'usageReport', component: UsageReportPageComponent, canActivate: [RoleGuard]},
+          { path: 'order', component: OrderPageComponent, canActivate: [RoleGuard]},
+          { path: 'nps', component: NPSPageComponent, canActivate: [RoleGuard]},
+          { path: 'produtividade', component: ProdutividadePageComponent, canActivate: [RoleGuard]},
+          { path: 'roomControl', component: RoomControlPage, canActivate: [RoleGuard]}
+        ]
+      }
     ]),
-    NvD3Module,
+    NvD3Module
   ],
   declarations: [
     CallbackComponent,
@@ -164,8 +98,9 @@ import { LoginComponent } from "../../login/login.component";
     PropertyAppComponent,
     HeaderStatusComponent,
     NotificationMessagesComponent,
-    NoRegisteredPropertiesComponent,
+    NoRegisteredPropertiesComponent
   ],
-  exports: [],
+  exports: [
+  ]
 })
-export class StarterPageModule {}
+export class StarterPageModule { }
