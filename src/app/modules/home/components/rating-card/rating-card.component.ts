ef161e932d7a2df714fbfc4cc99d5b216a9fc22c
Gilberto Santos <gilberto.junior@persisti.com.br>
2022-08-26 17:10:19 -0300

    bugfix: resolve conflict and layout rating card

--- src/app/modules/home/components/rating-card/rating-card.component.ts
+++ src/app/modules/home/components/rating-card/rating-card.component.ts
@@ -1,20 +1,16 @@
-
-import { map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import { IndicatorService } from './../../services/indicator/indicator.service';
-import { OnInit, OnDestroy } from '@angular/core';
-import { Component, Input } from '@angular/core';
-import { RealTime } from '../../../../abstracts/realTime';
-
+import { map } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { IndicatorService } from "./../../services/indicator/indicator.service";
+import { OnInit, OnDestroy } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { RealTime } from "../../../../abstracts/realTime";
 
 @Component({
   selector: "home-rating-card",
   templateUrl: "./rating-card.component.html",
-  styleUrls: ["./rating-card.component.scss"]
+  styleUrls: ["./rating-card.component.scss"],
 })
 export class RatingCardComponent implements OnInit, OnDestroy {
-
-
   @Input() indicatorGroup;
   @Input() propertyId;
 
@@ -25,17 +21,13 @@ export class RatingCardComponent implements OnInit, OnDestroy {
   count = 0;
   average = null;
 
-
-  constructor(private indicatorService: IndicatorService) {
-  }
+  constructor(private indicatorService: IndicatorService) {}
 
   ngOnInit() {
-
     if (this.indicatorGroup != null) {
       if (this.indicatorGroup.indicators != null) {
-
         //getting data for each indicator
-        this.indicatorGroup.indicators.forEach(indicator => {
+        this.indicatorGroup.indicators.forEach((indicator) => {
           const id = indicator.id;
 
           // first checks if realTime already exists for this indicator
@@ -48,9 +40,7 @@ export class RatingCardComponent implements OnInit, OnDestroy {
             //and add it to the realTimeMap
             this.realTimeMap[id] = currentRealTime;
 
-
             currentRealTime.startGettingRealTimeData(() => {
-              
               // getting indicators data
               this.getIndicatorData(id).subscribe((res) => {
                 indicator.data = res;
@@ -66,71 +56,71 @@ export class RatingCardComponent implements OnInit, OnDestroy {
               });
             });
           }
-
         });
       }
     }
   }
 
   ngOnDestroy(): void {
-    
     for (var indicatorId of Object.keys(this.realTimeMap)) {
       const realTime = this.realTimeMap[indicatorId];
       realTime.ngOnDestroy();
+    }
   }
-    
-  }
-
-
-
 
   getIndicatorData(indicatorId) {
-
     switch (indicatorId) {
-      case 'sold':
-        return this.calculateIndicator(this.indicatorService.getSoldIndicator(this.propertyId));
-      case 'presence':
-        return this.calculateIndicator(this.indicatorService.getPresenceIndicator(this.propertyId));
-      case 'savings':
-        return this.calculateIndicator(this.indicatorService.getSavingsIndicator());
-      case 'staffPresence':
-        return this.calculateIndicator(this.indicatorService.getStaffPresence());
-      case 'alerts':
-        return this.calculateIndicator(this.indicatorService.getAlertIndicator());
-      case 'waterLevel':
-        return this.calculateIndicator(this.indicatorService.getWaterLevelIndicator());
+      case "sold":
+        return this.calculateIndicator(
+          this.indicatorService.getSoldIndicator(this.propertyId)
+        );
+      case "presence":
+        return this.calculateIndicator(
+          this.indicatorService.getPresenceIndicator(this.propertyId)
+        );
+      case "savings":
+        return this.calculateIndicator(
+          this.indicatorService.getSavingsIndicator()
+        );
+      case "staffPresence":
+        return this.calculateIndicator(
+          this.indicatorService.getStaffPresence()
+        );
+      case "alerts":
+        return this.calculateIndicator(
+          this.indicatorService.getAlertIndicator()
+        );
+      case "waterLevel":
+        return this.calculateIndicator(
+          this.indicatorService.getWaterLevelIndicator()
+        );
       default:
         console.warn("Received unknown indicatorId '" + indicatorId + "'");
         return of({
-          label: '--',
-          value: "--"
+          label: "--",
+          value: "--",
         });
     }
   }
 
-
-
   calculateIndicator(indicatorServiceFunc: Observable<any>) {
+    return indicatorServiceFunc.pipe(
+      map((res) => {
+        if (this.total == null) {
+          this.total = 0;
+        }
+        const value = res.value;
 
-    return indicatorServiceFunc.pipe(map(((res) => {
-      if (this.total == null) {
-        this.total = 0;
-      }
-      const value = res.value;
+        if (value != null) {
+          this.total += value;
+          this.count++;
 
-      if (value != null) {
+          let avg = this.total / this.count;
 
-        this.total += value;
-        this.count++;
-
-        let avg = this.total / this.count;
-
-        this.average = Math.round(avg * 10) / 10;
-
-      }
-      return res;
-    })));
-
+          this.average = Math.round(avg * 10) / 10;
+        }
+        return res;
+      })
+    );
   }
-
 }
