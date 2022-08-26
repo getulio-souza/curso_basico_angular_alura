7f1907090cf8847f14a33a52de12269fffa5c967
João Machado <joao.vicente@evolutix.com.br>
2022-08-26 14:24:37 -0300

    fix: add inputs on table component

--- src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
+++ src/app/modules/order/components/order-productivity/order-table/order-table.component.ts
@@ -1,7 +1,5 @@
 import { OrderEventService } from "./../../../../../services/order/order-event.service";
-import { Component, OnInit } from "@angular/core";
-import { CollumnDefinition } from "../../../../dynamic-widgets/collapse-grid/collapse-grid.component";
-import { OrderWardQuantityDTO } from "../../../model/ordar-ward-quantity.dto";
+import { Component, Input, OnInit } from "@angular/core";
 import { Subject } from "rxjs";
 
 @Component({
@@ -10,36 +8,12 @@ import { Subject } from "rxjs";
   styleUrls: ["./order-table.component.scss"],
 })
 export class OrderTableComponent implements OnInit {
-  cols: CollumnDefinition[] = [
-    { field: "ward", header: "Ala" },
-    { field: "quantity", header: "Quantidade" },
-  ];
-
-  data = [];
+  @Input() cols: any[];
+  @Input() data: any[];
 
   private loadingSubject: Subject<void> = new Subject<void>();
 
   constructor(private orderEventService: OrderEventService) {}
 
   ngOnInit() {}
-
-  private topFiveWardsAndOppenedOrdersQuantityForTable(
-    ward,
-    category,
-    subject,
-    start,
-    end
-  ): void {
-    this.orderEventService
-      .topFiveWardsAndOppenedOrdersQuantity(ward, category, subject, start, end)
-      .subscribe((response: OrderWardQuantityDTO[]) => {
-        this.data = response.map((ward) => {
-          this.loadingSubject.next();
-          return Object.assign(
-            {},
-            { ward: ward.ward, quantity: ward.quantity }
-          );
-        });
-      });
-  }
 }
