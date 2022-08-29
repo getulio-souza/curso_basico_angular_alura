16a55817765a02a11ddd8af8738427be02427fc5
João Machado <joao.vicente@evolutix.com.br>
2022-08-29 10:45:22 -0300

    feature: create detail view component

--- src/app/modules/order/components/order-productivity/order-graphics-detail/order-graphics-detail.component.ts
+++ src/app/modules/order/components/order-productivity/order-graphics-detail/order-graphics-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { CHART_TYPES } from "./../../../../dynamic-widgets/charts/charts.component";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-order-graphics-detail',
-  templateUrl: './order-graphics-detail.component.html',
-  styleUrls: ['./order-graphics-detail.component.scss']
+  selector: "app-order-graphics-detail",
+  templateUrl: "./order-graphics-detail.component.html",
+  styleUrls: ["./order-graphics-detail.component.scss"],
 })
 export class OrderGraphicsDetailComponent implements OnInit {
+  @Input() opennedAdvance: any[] = [];
+  @Input() categoriesAdvance: any[] = [];
+  @Input() afterSlaAdvance: any[] = [];
+  @Input() wardsAdvance: any[] = [];
 
-  constructor() { }
+  CHART_TYPES = CHART_TYPES;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
