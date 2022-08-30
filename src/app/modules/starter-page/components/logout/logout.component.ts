57615de1bb5a569265a94afe94b2cef724893177
Getulio Souza <getuliosouza@live.com>
2022-08-30 16:35:54 -0300

    feature:user-name-date

--- src/app/modules/starter-page/components/logout/logout.component.ts
+++ src/app/modules/starter-page/components/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { AuthenticationService } from '../../../../shared/services/authentication.service';
 
 @Component({
   selector: 'app-logout-component',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  username:string;
+  lastLogin: string;
+
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.username = this.getUserName();
+    this.lastLogin = this.getUserLastLogin();
   }
 
+ getUserName(): string{
+  return this.authenticationService.getUsername();
+ }
+
+ getUserLastLogin(): string{
+    this.authenticationService.getLastLoginDate();
+    return this.formatDateLong(this.authenticationService.getLastLoginDate())
+
+ }
+
+ formatDateLong(date: Date): string {
+  return moment(date).format('DD/MM/YYYY - HH:mm') + 'h';
+}
 }
